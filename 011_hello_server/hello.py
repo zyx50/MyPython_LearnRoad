a5db9ebc7459cd74c640cbca6654d8c84c89aeb6
zyxyx_50 <345355384@qq.com>
2016-11-08 17:06:03 +0800

    hello server

---
+++ 011_hello_server/hello.py
@@ -0,0 +1,5 @@
+__author__ = 'zyx'
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return '<h1>Hello, %s!</h1>' %(environ ['PATH_INFO'][1:] or 'web')
+
