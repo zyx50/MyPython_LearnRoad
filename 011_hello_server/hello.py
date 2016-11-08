ccf3904582607e7562c1f474e47e7125099fbea5
zyxyx_50 <345355384@qq.com>
2016-11-08 16:43:38 +0800

    删除

--- 011_hello_server/hello.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'zyx'
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    return '<h1>Hello, %s!</h1>' %(environ ['PATH_INFO'][1:] or 'web')
-
